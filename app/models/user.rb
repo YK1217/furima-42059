83efb431705b3c8f5d58127374d10c3f52bdd6a7
YK1217 <bean.sousaku@gmail.com>
2025-09-09 14:56:18 +0900

    バリデーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,17 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  with options presence: true do
+    validates :nickname
+    validates :birthday
+    with options format: {with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: '全角文字を使用してください'} do
+      validates :first_name
+      validates :second_name
+    end
+    with options format: {with: /\A[ァ-ヶー]+\z/, message: '全角カタカナを使用してください'} do
+      validates :first_name_kana
+      validates :second_name_kana
+    end
+  end
 end
