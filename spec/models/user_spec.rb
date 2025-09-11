92907ba2255433ebb0d499c12be1033f423e2757
YK1217 <bean.sousaku@gmail.com>
2025-09-11 14:54:43 +0900

    FactoryBotの設定

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @user = FactoryBot.build(:user)
+  end
+
+  describe 'ユーザー新規登録' do
+    context '新規登録できるとき'
+      it 'nicknameとemail、password、password_confirmation、first_name、second_name、first_name_kana、second_name_kana、birthdayが存在すれば登録できる' do
+        binding.pry
+        #expect(@user).to be_valid
+      end
+    end
+  end
 end
