1ca46b7e9c9bad82eff71295d167b3cbb0b1edb1
YK1217 <bean.sousaku@gmail.com>
2025-09-11 10:25:26 +0900

    誤記の修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,14 +4,14 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  with options presence: true do
+  with_options presence: true do
     validates :nickname
     validates :birthday
-    with options format: {with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: '全角文字を使用してください'} do
+    with_options format: {with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: '全角文字を使用してください'} do
       validates :first_name
       validates :second_name
     end
-    with options format: {with: /\A[ァ-ヶー]+\z/, message: '全角カタカナを使用してください'} do
+    with_options format: {with: /\A[ァ-ヶー]+\z/, message: '全角カタカナを使用してください'} do
       validates :first_name_kana
       validates :second_name_kana
     end
