e5db95310c4acd42e189c687b2c84a6b2654dc93
YK1217 <bean.sousaku@gmail.com>
2025-09-22 10:52:16 +0900

    「カテゴリー」の選択の実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,16 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+
   belongs_to :user
   has_one_attached :image
+
+  with_options presence: true do
+    validates :name
+    validates :caption
+    validates :price
+  end
+
+  #ジャンルの選択が「---」の時は保存できないようにする
+  validates :category_id, numericality: { other_than: 1 , message: "can't be blank"}
 end
