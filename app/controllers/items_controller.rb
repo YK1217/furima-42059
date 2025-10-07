dace73f848220dd44a0d372210b3e037cecbacfa
YK1217 <bean.sousaku@gmail.com>
2025-10-07 14:58:20 +0900

    ログアウト状態だったり商品の出品者ではない場合にトップページへ遷移するよう設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -24,6 +24,10 @@ class ItemsController < ApplicationController
 
   def edit
     @item = Item.find(params[:id])
+
+    unless user_signed_in? && @item.user_id == current_user.id
+      redirect_to root_path
+    end
   end
 
   def update
